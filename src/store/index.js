ad9dbd3101ee583c904ab5e58ce65fa4d13103a0
Peter ISKANDER <peter.iskander@valeo.com>
2024-08-28 13:00:07 +0300

    firebase removal

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,4 @@
 import { createStore } from 'vuex'
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
-import { collection, doc, getFirestore, onSnapshot, setDoc } from 'firebase/firestore';
-
 function areCredentialsValid(credentials) {
   if (!credentials) return false;
   const { token, expiry } = credentials;
@@ -21,102 +18,22 @@ function getStoredCredentials() {
 export default createStore({
   state: {
     user: null,
-    quests: [],
-    loading: false,
-    failed: false,
   },
   getters: {
     getUser: (state) => state.user,
-    getQuests: (state) => state.quests,
     getLoading: (state) => state.loading,
     getFailed: (state) => state.failed
   },
   mutations: {
     setUser: (state, user) => (state.user = user),
-    setQuests: (state, quests) => (state.quests = quests),
     setLoading: (state, loading) => (state.loading = loading),
     setFailed: (state, failed) => (state.failed = failed)
   },
   actions: {
-    async login({ commit }) {
-      commit('setFailed', false);
-      commit('setLoading', true);
-
-      const storedCredentials = getStoredCredentials();
-      if (areCredentialsValid(storedCredentials)) {
-        commit('setUser', storedCredentials.user);
-        commit('setLoading', false);
-        return Promise.resolve(storedCredentials.user);
-      }
-
-      const provider = new GoogleAuthProvider();
-      return signInWithPopup(getAuth(), provider).then(async res => {
-        const firestore = getFirestore();
-        const userCollectionReference = collection(firestore, 'users');
-
-        let allUsers = [];
-        return new Promise((resolve, reject) => {
-          onSnapshot(userCollectionReference, snapshot => {
-            allUsers = snapshot.docs.map(doc => doc.id);
-            let newUser = !allUsers.includes(res.user.uid);
-            let user = null;
-            if (newUser) {
-              user = {
-                uid: res.user.uid,
-                name: res.user.displayName,
-                assignedQuestId: "",
-                isTeamLead: false,
-                email: res.user.email
-              }
-              setDoc(doc(firestore, "users", res.user.uid), user, { merge: true }).then(() => {
-                const credentials = {
-                  user,
-                  token: res.user.accessToken,
-                  expiry: new Date().getTime() + 3600 * 1000 // 1 hour expiry
-                };
-                saveCredentials(credentials);
-                commit('setUser', user);
-                resolve(user);
-              }).catch(reject);
-            } else {
-              const userDoc = doc(userCollectionReference, res.user.uid);
-              onSnapshot(userDoc, snapshot => {
-                const data = snapshot.data();
-                user = {
-                  uid: res.user.uid,
-                  email: res.user.email,
-                  ...data
-                }
-                const credentials = {
-                  user,
-                  token: res.user.accessToken,
-                  expiry: new Date().getTime() + 3600 * 1000 // 1 hour expiry
-                };
-                saveCredentials(credentials);
-                commit('setUser', user);
-                resolve(user);
-              }, reject);
-            }
-          }, reject);
-        });
-      }).catch(err => {
-        console.error(err);
-        commit('setFailed', true);
-      }).finally(() => {
-        commit('setLoading', false);
-      });
-    },
+    async login({ commit }) {},
     updateUser({ commit }, user) {
       commit('setUser', user);
     },
-    setQuests({ commit }) {
-      const firestore = getFirestore();
-      const questsCollectionReference = collection(firestore, 'quests');
-      onSnapshot(questsCollectionReference, snapshot => {
-        const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        commit('setQuests', data);
-      });
-    }
   },
   modules: {
   }
