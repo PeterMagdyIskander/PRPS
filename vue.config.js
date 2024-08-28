56049558be07518593f46cf598e54a256e826e9f
Peter ISKANDER <peter.iskander@valeo.com>
2024-08-28 12:44:02 +0300

    trying to make image not blurry

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-    rules: [
-        {
-            test: /\.svg$/,
-            use: [
-                {
-                    loader: 'svg-url-loader',
-                    options: {
-                        limit: 10000,
-                        noquotes: true,
-                    },
-                },
-                {
-                    loader: 'svgo-loader',
-                    options: {
-                        plugins: [
-                            { removeViewBox: false },
-                            { cleanupIDs: false },
-                            { removeDimensions: false },
-                        ],
-                    },
-                },
-            ],
-        },
-    ],
 })
