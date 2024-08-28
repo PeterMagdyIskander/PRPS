6ee70ed85e90e8ac2cc364b89f5dbe651babe873
Peter ISKANDER <peter.iskander@valeo.com>
2024-08-28 12:28:34 +0300

    trying to make image not blurry

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@
         challenges and opportunities in your career journey.
       </h1>
       <button class="header-call-to-action">Get started</button>
-      <img class="header-device" src="@/assets/images/device-mobile-view.png" loading="eager" alt="device-mobile">
+      <img class="header-device" src="@/assets/images/device-mobile-view.svg" loading="eager" alt="device-mobile">
       <img class="header-shape" src="@/assets/images/shape-mobile-view.svg" loading="eager" alt="shape-mobile">
     </div>
   </div>
@@ -75,6 +75,8 @@ export default {
   }
 
   &-device {
+    width: fit-content;
+    max-width: fit-content;
     margin-top: -20px;
     position: relative;
     z-index: 1;
