8a2227717b6bc5659bc2eb0378a1abf5fdd90617
Peter ISKANDER <peter.iskander@valeo.com>
2024-08-28 12:47:59 +0300

    trying to make image not blurry

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-    rules: [
-        {
-            test: /\.svg$/,
-            use: [
-                {
-                    loader: 'svg-url-loader',
-                    options: {
-                        limit: 10000,
-                        noquotes: true,
-                    },
-                },
-                {
-                    loader: 'svgo-loader',
-                    options: {
-                        plugins: [
-                            { removeViewBox: false },
-                            { cleanupIDs: false },
-                            { removeDimensions: false },
-                        ],
-                    },
-                },
-            ],
-        },
-    ],
+  ]
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-})
+
+  chainWebpack: config => {
+    // Clear existing SVG rule to add custom loader
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+
+    // Add vue-svg-loader with SVGO options
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .tap(options => ({
+        // Ensure viewBox and other critical attributes are preserved
+        svgo: {
+          plugins: [
+            { removeViewBox: false },
+            { cleanupIDs: false },
+            { removeDimensions: false },
+          ],
+        },
+      }));
+  },
+});
