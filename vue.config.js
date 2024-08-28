d3ad02f4c0661f34d419c0234176bf48fbd47f2c
Peter ISKANDER <peter.iskander@valeo.com>
2024-08-28 12:41:28 +0300

    trying to make image not blurry

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  module: {
     rules: [
         {
             test: /\.svg$/,
@@ -26,5 +25,4 @@ module.exports = defineConfig({
             ],
         },
     ],
-},
 })
