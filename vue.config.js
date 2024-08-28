bbbcd47593f508e866758fd43ef034986836663d
Peter ISKANDER <peter.iskander@valeo.com>
2024-08-28 13:11:44 +0300

    3ala allah

--- vue.config.js
+++ vue.config.js
@@ -7,19 +7,5 @@ module.exports = defineConfig({
     // Clear existing SVG rule to add custom loader
     const svgRule = config.module.rule('svg');
     svgRule.uses.clear();
-
-    // Add vue-svg-loader with SVGO options
-    svgRule
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader')
-      .tap(options => ({
-        svgo: {
-          plugins: [
-            { removeViewBox: false },
-            { cleanupIDs: false },
-            { removeDimensions: false },
-          ],
-        },
-      }));
   },
 });
