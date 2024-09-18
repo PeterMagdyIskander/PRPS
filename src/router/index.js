c7effd55f7681d09f53f1193031e48b99d2e919d
Peter Magdy <petermagdyiskander@gmail.com>
2024-09-18 23:53:24 +0300

    login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import store from "../store/index.js";
 import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
 ];
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container">
+        <button class="login">
+            Login
+        </button>
+        <div class="separator">
+            <div class="line"></div>
+            <p>Or</p>
+            <div class="line"></div>
+        </div>
+        <div class="login-provider">
+            <img src="@/assets/images/google.svg" alt="">
+            <p>continue with Google</p>
+        </div>
+        <div class="login-provider"><img src="@/assets/images/facebook.svg" alt="">
+            <p>continue with Facebook</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginView',
+
+}
+</script>
+
+<style scoped lang="scss">
+* {
+    padding: 0;
+    margin: 0;
+}
+
+.container {
+    .separator {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        color: #535A5F;
+        font-family: 'Poppins-Regular';
+        max-width: 342px;
+        .line {
+            height: 1px;
+            width: 40%;
+            background-color: #DDE5E9;
+        }
+    }
+
+    .login {
+        all: unset;
+        cursor: pointer;
+        color: #fff;
+        background-color: #0044F1;
+        padding: 10px 147px;
+        font-size: 18px;
+        font-family: 'Poppins-Regular';
+        border-radius: 12px;
+        max-width: 342px;
+    }
+
+    .login-provider {
+        cursor: pointer;
+        background-color: rgba($color: #0044F1, $alpha: 0.08);
+        display: flex;
+        align-items: center;
+        column-gap: 12px;
+        padding: 15px 48px;
+        font-size: 18px;
+        font-family: 'Poppins-Regular';
+        color: #535A5F;
+        border-radius: 12px;
+        max-width: 342px;
+    }
+}
+</style>
